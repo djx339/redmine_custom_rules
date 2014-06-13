9779fc283ccd0886b1785f1a4a80e04f98ae074c
djx339 <djx339@gmail.com>
2014-06-13 13:44:48 +0800

    Finished Support #3709
    Add 'Verify count' field
      * Read-only field which can only be modified by Redmine self and admin
      * Increased automatically when Bug changed from 'Resolved' to 'Verify Failed'

---
+++ init.rb
@@ -0,0 +1,25 @@
+Redmine::Plugin.register :custom_rules do
+  name 'Custom Rules plugin'
+  author 'Ding jianxiong'
+  description 'This is a plugin for our custom rules about workflow'
+  version '0.0.1'
+  url 'http://redmine.socialworks.mobi/users/3'
+  author_url 'http://redmine.socialworks.mobi/users/3'
+end
+
+
+module CustomRules
+  class Hooks < Redmine::Hook::ViewListener
+    def controller_issues_edit_before_save(context={})
+      
+      # Auto incrase 'Verify Count' when the issue verify faild.
+      # Also can see the detail vai http://redmine.socialworks.mobi/issues/3709
+      if context[:issue].status.name == 'Verify Failed' && context[:issue].status_id_changed?
+        context[:issue].custom_field_values.each do |custom_value|
+          custom_value.value = (custom_value.value.to_i + 1).to_s if custom_value.custom_field.name == 'Verify Count'
+        end
+      end
+
+    end
+  end
+end
