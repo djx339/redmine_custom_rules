a884528d4d61fb6b204749ae72be3c142c12c600
djx339 <djx339@gmail.com>
2014-06-13 14:18:10 +0800

    Change the plugin url with truth url.
    Change the rule: only from Resolved to Verify Faild

--- init.rb
+++ init.rb
@@ -3,7 +3,7 @@ Redmine::Plugin.register :custom_rules do
   author 'Ding jianxiong'
   description 'This is a plugin for our custom rules about workflow'
   version '0.0.1'
-  url 'http://redmine.socialworks.mobi/users/3'
+  url 'https://github.com/daniel-djx/redmine_custom_rules.git'
   author_url 'http://redmine.socialworks.mobi/users/3'
 end
 
@@ -14,7 +14,7 @@ module CustomRules
       
       # Auto incrase 'Verify Count' when the issue verify faild.
       # Also can see the detail vai http://redmine.socialworks.mobi/issues/3709
-      if context[:issue].status.name == 'Verify Failed' && context[:issue].status_id_changed?
+      if context[:issue].status_id_changed? && context[:issue].status_was.name == 'Resolved' && context[:issue].status.name == 'Verify Failed'
         context[:issue].custom_field_values.each do |custom_value|
           custom_value.value = (custom_value.value.to_i + 1).to_s if custom_value.custom_field.name == 'Verify Count'
         end
