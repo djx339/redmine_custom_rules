f87747fb47e5910bf43bbf441c28dbfc503edf7e
djx339 <djx339@gmail.com>
2014-06-16 15:53:47 +0800

    Finished Support #3710
    Set field regression to true automatically when 'Caused by''s tracker is Bug.

--- init.rb
+++ init.rb
@@ -3,7 +3,7 @@ Redmine::Plugin.register :custom_rules do
   author 'Ding jianxiong'
   description 'This is a plugin for our custom rules about workflow'
   version '0.0.1'
-  url 'https://github.com/daniel-djx/redmine_custom_rules.git'
+  url 'https://github.com/daniel-djx/redmine_custom_rules'
   author_url 'http://redmine.socialworks.mobi/users/3'
 end
 
@@ -11,15 +11,53 @@ end
 module CustomRules
   class Hooks < Redmine::Hook::ViewListener
     def controller_issues_edit_before_save(context={})
-      
       # Auto incrase 'Verify Count' when the issue verify faild.
-      # Also can see the detail vai http://redmine.socialworks.mobi/issues/3709
+      # Also can see the detail via http://redmine.socialworks.mobi/issues/3709
       if context[:issue].status_id_changed? && context[:issue].status_was.name == 'Resolved' && context[:issue].status.name == 'Verify Failed'
         context[:issue].custom_field_values.each do |custom_value|
           custom_value.value = (custom_value.value.to_i + 1).to_s if custom_value.custom_field.name == 'Verify Count'
         end
       end
 
+      # Set Regression field value automated.
+      set_regression_field_value(context)
     end
+
+    def controller_issues_new_before_save(context={})
+      # Set Regression field value automated.
+      set_regression_field_value(context)
+    end
+
+    private
+
+    # Set to true automatically when 'Caused by' issue's tracker is Bug.
+    # Default value is false.
+    # The detail can be seen via http://redmine.socialworks.mobi/issues/3710
+    def set_regression_field_value(context={})
+      lissue = context[:issue]
+
+      # Make sure current issue custom fields include 'Caused by' and 'Regression'.
+      # And 'Caused by' should be a number and 'Regression's value should be false.
+      # Note: 'true' is '1' and 'false' is '0' in redmine custom field.
+      field_caused_by = field_regression = nil
+      lissue.custom_field_values.each do |custom_value|
+        field_caused_by = custom_value if custom_value.custom_field.name == 'Caused by' &&
+                                          custom_value.value =~ /\d+/
+        field_regression = custom_value if custom_value.custom_field.name == 'Regression' &&
+                                           custom_value.value == '0'    # See above Note.
+        break if field_caused_by and field_regression
+      end
+
+      if field_caused_by && field_regression
+          begin
+            caused_by_issue = Issue.find(field_caused_by.value)
+          rescue ActiveRecord::RecordNotFound
+            caused_by_issue = nil
+          end
+          # See above note.
+          field_regression.value = '1' if caused_by_issue && caused_by_issue.tracker.name == 'Bug'
+      end
+    end
+
   end
 end
